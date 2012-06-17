766d037a467997ce1f8ae3c0cac499778639c7ce
Ubbi Dubbi <dubbiubbi@gmail.com>
2012-06-17 02:37:59 -0400

    first commit

---
+++ src/main/java/com/gmail/dubbiubbi/aussiespeak/AussieSpeak.java
@@ -0,0 +1,45 @@
+package com.gmail.dubbiubbi.aussiespeak;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.ChatColor;
+
+public class AussieSpeak extends JavaPlugin implements Listener {
+	private boolean inUse = false;
+	
+	@Override
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+
+		this.getLogger().info("AussieSpeak loaded!");
+	}
+
+	@Override
+	public void onDisable() {
+		this.getLogger().info("AussieSpeak unloaded!");
+	}
+
+	@EventHandler
+	public void onPlayerChat(PlayerChatEvent event) {
+		String message = event.getMessage();
+		String messageToSend = new String(ChatColor.DARK_RED+"You do not have permission.");
+		Player player = event.getPlayer();
+		if(message.equals("/aussiespeak")){
+			if(player.isOp()){
+					inUse = !inUse;
+			}else{
+				player.sendMessage(messageToSend);
+			}
+		}
+		if(inUse){
+			message.replaceAll(" ", " cunt ");
+			message.replaceAll(".", ", mate.");
+		}	
+			
+		//message = ChatColor.GREEN + message;
+		event.setMessage(message);
+	}
+}
