3917e2e1c2bfbdcf7b6d2afdc9e0f10fac7a3228
Ubbi Dubbi <dubbiubbi@gmail.com>
2012-06-18 15:21:29 -0400

    Added Comments

--- src/main/java/com/gmail/dubbiubbi/aussiespeak/AussieSpeak.java
+++ src/main/java/com/gmail/dubbiubbi/aussiespeak/AussieSpeak.java
@@ -13,26 +13,29 @@ public class AussieSpeak extends JavaPlugin implements Listener {
 	
 	@Override
 	public void onEnable() {
+		//registers the events so event handling can be done.
 		getServer().getPluginManager().registerEvents(this, this);
-
+		//prints to the logger that the plugin was loaded.
 		this.getLogger().info("AussieSpeak loaded!");
-		
+		//Sets the description for the command aussiespeak.
 		this.getCommand("aussiespeak").setDescription("Toggles pluin feature on and off.");
-		
+		//Sets the permission for the command aussiespeak so only ops can use it.
 		this.getCommand("aussiespeak").setPermission("op");
-		
+		//Prints a custom message when someone who doesn't have permission tries to use the command.
 		this.getCommand("aussiespeak").setPermissionMessage(ChatColor.DARK_RED + "You don't have permission to use this command.");
 	}
 
 	@Override
 	public void onDisable() {
+		//Prints to the logger when the plugin is unloaded. 
 		this.getLogger().info("AussieSpeak unloaded!");
 	}
 	
 	
-	
+	//Method that tells what to do when the aussiespeak command is called.
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
     	if(cmd.getName().equalsIgnoreCase("aussiespeak")){
+    			//Boolean value to toggle feature on and off.
     			inUse = !inUse;
         		return true;
     	}
@@ -41,7 +44,9 @@ public class AussieSpeak extends JavaPlugin implements Listener {
 
 	@EventHandler
 	public void onPlayerChat(PlayerChatEvent event) {
+		//When player uses chat this code runs. Checks to see if feature is enabled.
 		if(inUse){
+			//If the feature is turned on this code runs which makes cunt every other word and ends messages with mate.
 			String message = event.getMessage();
 			message = message.replaceAll(" ", " cunt ");
 			if(message.contains(".")){
